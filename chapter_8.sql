beaa89bf6a93189ddbd741fdfcf8460a2b0e3612
zhangshuo1996 <18796360983zs@gmail.com>
2021-06-03 21:03:52 +0800

    FOR 循环 continue

--- chapter_8.sql
+++ chapter_8.sql
@@ -78,3 +78,214 @@ END;
 select empno, hiredate, job, sal, deptno
 from emp_pl
 where deptno > 20;
+
+
+-- 8-14
+set serveroutput on
+set verify off
+declare
+    v_deptno dept.deptno%TYPE;
+    v_loc dept.loc%TYPE := '&p_loc';
+    v_max_num NUMBER(2) := &p_max_num;
+begin
+    FOR i IN 1..v_max_num LOOP
+        INSERT INTO dept_pl(deptno, loc)
+        VALUES(dept_id_sequence.NEXTVAL, v_loc);
+    END LOOP;
+END;
+/
+
+
+-- 8-15
+select *
+from dept_pl;
+
+-- 8-16
+set serveroutput on
+set verify off
+declare 
+    v_empno     emp_pl.empno%TYPE;
+    v_deptno    emp_pl.deptno%TYPE := &p_deptno;
+    v_sal       emp_pl.sal%TYPE := &p_sal;
+    v_hiredate  emp_pl.hiredate%TYPE := SYSDATE;
+    v_job       emp_pl.job%TYPE := '&p_job';
+    v_max_num   NUMBER(2) := &p_max_num;
+begin
+    select max(empno) into v_empno
+    from emp_pl;
+
+    FOR i IN 1..v_max_num LOOP
+        INSERT INTO emp_pl(empno, hiredate, job, sal, deptno)
+        VALUES((v_empno + i), v_hiredate, v_job, v_sal, v_deptno);
+    END LOOP;
+END;
+/
+
+-- 
+select empno, hiredate, job, sal, deptno
+from emp_pl
+where deptno > 20;
+
+
+-- 8-20
+set serveroutput on
+set verify off
+declare 
+    v_empno     emp_pl.empno%TYPE;
+    v_deptno    emp_pl.deptno%TYPE := &p_deptno;
+    v_sal       emp_pl.sal%TYPE := &p_sal;
+    v_hiredate  emp_pl.hiredate%TYPE := SYSDATE;
+    v_job       emp_pl.job%TYPE := '&p_job';
+    v_max_num   NUMBER(2) := &p_max_num;
+begin
+    select max(empno) into v_empno
+    from emp_pl;
+
+    FOR i IN REVERSE 1..v_max_num LOOP
+        INSERT INTO emp_pl(empno, hiredate, job, sal, deptno)
+        VALUES((v_empno + i), v_hiredate, v_job, v_sal, v_deptno);
+    END LOOP;
+END;
+/
+
+
+-- 8-22 循环嵌套标识
+declare
+    v_total     NUMBER := 0;
+    v_factorial NUMBER := 1;
+    v_num       NUMBER := &p_num;
+begin
+    <<Outer_Loop>>
+    FOR i IN 1..v_num LOOP
+        v_total := v_total + i;
+        DBMS_OUTPUT.PUT_LINE('1 ~ ' || i || '的自然数总和是 ： ' || v_total);
+        <<Inner_Loop>>
+        FOR j IN 1..v_num LOOP  
+            EXIT Inner_Loop WHEN i+j > 4;
+            v_factorial := v_factorial * j;
+            DBMS_OUTPUT.PUT_LINE('自然数 ' || j || '的阶乘是: ' || v_factorial);
+        END LOOP;
+        v_factorial := 1;
+    END LOOP Outer_Loop;
+END;
+/
+
+-- 8-24
+set serveroutput on
+declare
+    v_total     NUMBER := 0;
+    v_factorial NUMBER := 1;
+    v_num       NUMBER := &p_num;
+begin
+    <<Outer_Loop>>
+    FOR i IN 1..v_num LOOP
+        v_total := v_total + i;
+        DBMS_OUTPUT.PUT_LINE('1 ~ ' || i || '的自然数总和是 ： ' || v_total);
+        <<Inner_Loop>>
+        FOR j IN 1..v_num LOOP  
+            EXIT Outer_Loop WHEN i+j > 4;
+            v_factorial := v_factorial * j;
+            DBMS_OUTPUT.PUT_LINE('自然数 ' || j || '的阶乘是: ' || v_factorial);
+        END LOOP;
+        v_factorial := 1;
+    END LOOP Outer_Loop;
+END;
+/
+
+-- 8-25
+set verify off
+set serveroutput on
+declare
+    v_total     NUMBER := 0;
+    v_factorial NUMBER := 1;
+    v_num       NUMBER := &p_num;
+begin
+    <<Outer_Loop>>
+    FOR i IN 1..v_num LOOP
+        v_total := v_total + i;
+        DBMS_OUTPUT.PUT_LINE('1 ~ ' || i || '的自然数总和是 ： ' || v_total);
+        <<Inner_Loop>>
+        FOR j IN 1..v_num LOOP  
+            EXIT Outer_Loop WHEN i+j > 4;
+            v_factorial := v_factorial * j;
+            DBMS_OUTPUT.PUT_LINE('自然数 ' || j || '的阶乘是: ' || v_factorial);
+        END LOOP;
+        v_factorial := 1;
+    END LOOP Outer_Loop;
+END;
+/
+
+-- 8-26
+set verify off
+set serveroutput on
+declare
+    v_total     NUMBER := 0;
+    v_factorial NUMBER := 1;
+    v_num       NUMBER := &p_num;
+begin
+    <<Outer_Loop>>
+    FOR i IN 1..v_num LOOP
+        v_total := v_total + i;
+        DBMS_OUTPUT.PUT_LINE('1 ~ ' || i || '的自然数总和是 ： ' || v_total);
+        <<Inner_Loop>>
+        FOR j IN 1..v_num LOOP  
+            CONTINUE Outer_Loop WHEN i+j > 4;
+            v_factorial := v_factorial * j;
+            DBMS_OUTPUT.PUT_LINE('自然数 ' || j || '的阶乘是: ' || v_factorial);
+        END LOOP;
+        v_factorial := 1;
+    END LOOP Outer_Loop;
+END;
+/
+
+
+-- 8-27
+set verify off
+set serveroutput on
+declare
+    v_total     NUMBER := 0;
+    v_factorial NUMBER := 1;
+    v_num       NUMBER := &p_num;
+begin
+    <<Outer_Loop>>
+    FOR i IN 1..v_num LOOP
+        v_factorial := 1;
+        v_total := v_total + i;
+        DBMS_OUTPUT.PUT_LINE('1 ~ ' || i || '的自然数总和是 ： ' || v_total);
+        <<Inner_Loop>>
+        FOR j IN 1..v_num LOOP  
+            CONTINUE Outer_Loop WHEN i+j > 4;
+            v_factorial := v_factorial * j;
+            DBMS_OUTPUT.PUT_LINE('自然数 ' || j || '的阶乘是: ' || v_factorial);
+        END LOOP;
+    END LOOP Outer_Loop;
+END;
+/
+
+
+-- 8-27
+set verify off
+set serveroutput on
+declare
+    v_total     NUMBER := 0;
+    v_num       NUMBER := &p_num;
+begin
+    <<Outer_Loop>>
+    FOR i IN 1..v_num LOOP
+        v_total := v_total + i;
+        DBMS_OUTPUT.PUT_LINE('1 ~ ' || i || '的自然数总和是 ： ' || v_total);
+        begin   
+            declare
+                v_factorial NUMBER := 1;
+            begin
+                <<Inner_Loop>>
+                FOR j IN 1..v_num LOOP  
+                    CONTINUE Outer_Loop WHEN i+j > 4;
+                    v_factorial := v_factorial * j;
+                    DBMS_OUTPUT.PUT_LINE('自然数 ' || j || '的阶乘是: ' || v_factorial);
+                END LOOP;
+            END;
+        END;
+    END LOOP Outer_Loop;
+END;
+/
